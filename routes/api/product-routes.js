f85a42fcdb5873397e4d4bd8ebad0fa30ff500b5
smrsun4 <smrsun4@gmail.com>
2024-09-09 20:27:09 -0400

    fixed product route product.create and removed a tag id in the wrong spot

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -47,25 +47,25 @@ router.get('/:id', async (req, res) => {
   }
 */
 router.post('/', async (req, res) => {
-  /*
-  try {
-    const newProduct = await Product.create({
-      product_name: req.body.product_name,
-      price: req.body.price,
-      stock: req.body.stock,
-      category_id: req.body.category_id,
-      tagIds: req.body.tagIds
-    });
-    res.status(200).json(newProduct);
-    } catch (err) {
-      console.log("First Er: ", err);
-      res.status(400).json(err)
-    }
-      */
+ 
+  // try {
+  //   const newProduct = await Product.create({
+  //     product_name: req.body.product_name,
+  //     price: req.body.price,
+  //     stock: req.body.stock,
+  //     category_id: req.body.category_id,
+  //     tagIds: req.body.tagIds
+  //   });
+  //   res.status(200).json(newProduct);
+  //   } catch (err) {
+  //     console.log("First Er: ", err);
+  //     res.status(400).json(err)
+  //   }
+    
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
